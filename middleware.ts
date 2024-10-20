19a96da76eafc86c2c6b69a068a810772e51e042
mayur-keswani <mayurkeswani2001@gmail.com>
2024-10-20 12:58:55 +0530

    build issue

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest) {
-    if (typeof window !== "undefined") {
+    // if (typeof window !== "undefined") {
         const auth: any = req.cookies.get('auth')?.value
         let parsedAuth;
         try {
@@ -13,12 +13,13 @@ export function middleware(req: NextRequest) {
         }
         // Redirect to login if the user is not logged in or if the auth data is invalid
         if (!parsedAuth || !parsedAuth.isLoggedIn) {
+            
             return NextResponse.redirect(new URL('/login', req.url));
         }
 
         // Continue to the requested page if the user is authenticated
         return NextResponse.next();
-    }
+    // }
 }
 
 export const config = {
